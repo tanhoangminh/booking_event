4915ceca5c384de8eb98741b31c5c128ef06e004
tanhoang1603 <tan.hoang@anfin.vn>
2023-09-08 15:59:26 +0700

    first commit

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -37,7 +37,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Booking demo'),
+        title: const Text('Booking event demo'),
       ),
       body: BookingWidget(
         controller: controller,
